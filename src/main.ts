e9c85231d2e3472192bcc0282b8f7d51027f54d7
manickamj <jegan.manickam@agilsys.com>
2024-11-07 17:55:58 +0000

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
@@ -9,10 +10,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
+    <p *ngFor="let item of data">{{item}}</p>
   `,
+  imports: [CommonModule],
 })
 export class App {
   name = 'Angular';
+  data = ['Item 1', 'Item 2', 'Item 3'];
 }
 
 bootstrapApplication(App);
