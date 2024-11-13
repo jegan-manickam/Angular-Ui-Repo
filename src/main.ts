83d7f43c85b75ee3719f680d11f33c1dd00fd25a
jegan-manickam <jegansyncdata@gmail.com>
2024-11-13 23:37:21 +0530

    add app config

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-    <p *ngFor="let item of data">{{item}}</p>
-  `,
-  imports: [CommonModule],
-})
-export class App {
-  name = 'Angular';
-  data = ['Item 1', 'Item 2', 'Item 3'];
-}
-
-bootstrapApplication(App);
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
