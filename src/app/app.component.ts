b3f3bdfc6be7633b1748ce475e03fddd7dc5ef96
jegan-manickam <jegansyncdata@gmail.com>
2024-11-19 22:55:13 +0530

    Added routing and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet]
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import {Routes} from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'links',
+        loadComponent: () => import('./component/link/link.component').then(render => render.LinkComponent)
+    },
+    {
         path: 'input',
         loadComponent: () => import('./component/input/input.component').then(render => render.InputComponent)
     },
-    { path: '',   redirectTo: '/input', pathMatch: 'full' },
+    {
+        path: 'componentCommunication',
+        loadComponent: () => import('./component/component-communication/component-communication.component').then(render => render.ComponentCommunication)
+    },
+    { path: '',   redirectTo: 'links', pathMatch: 'full' },
     //{ path: '**', component: PageNotFoundComponent },
 ];
