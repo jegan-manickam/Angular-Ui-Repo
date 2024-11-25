39ead0ba872cc935b17c6202e897a06ba67ffc61
jegan-manickam <jegansyncdata@gmail.com>
2024-11-26 00:22:13 +0530

    Added angular Form component

---
+++ src/app/component/angular-form/angular-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-form',
+  imports: [],
+  templateUrl: './angular-form.component.html',
+  styleUrls: ['./angular-form.component.scss']
+})
+export class AngularFormComponent {
+
+}
