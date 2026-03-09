deffb69ae0ceaab613d147609ee9706a2e5d007f
jegan-manickam <jegansyncdata@gmail.com>
2026-03-09 14:32:02 +0530

    firebase config added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
 import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app'
+import {getFirestore, provideFirestore} from '@angular/fire/firestore'
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+  ]
 };
